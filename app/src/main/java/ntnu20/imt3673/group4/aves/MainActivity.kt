5990c0353a989910ce8f01dce7a327e2173123ae
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-04 11:28:26 +0200

    Added project files

---
+++ app/src/main/java/ntnu20/imt3673/group4/aves/MainActivity.kt
@@ -0,0 +1,12 @@
+package ntnu20.imt3673.group4.aves
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
