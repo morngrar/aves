a3f24b0dd9d5c61cd5b20bbe35eae057ac50ed2e
charleyv <charleyv@stud.ntnu.no>
2020-04-15 16:09:03 +0200

    Functioning nav drawer without fragments

--- app/src/main/java/ntnu20/imt3673/group4/aves/MainActivity.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/MainActivity.kt
@@ -2,15 +2,38 @@ package ntnu20.imt3673.group4.aves
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import ntnu20.imt3673.group4.aves.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.observation_view)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
+        this.drawer = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState()
+    }
 
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/ntnu20/imt3673/group4/aves/viewmodels/AvesViewModel.kt
+++
@@ -1,9 +0,0 @@
-package ntnu20.imt3673.group4.aves.viewmodels
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MyViewModel : ViewModel() {
-
-}
