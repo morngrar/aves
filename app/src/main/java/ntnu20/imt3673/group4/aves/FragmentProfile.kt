32a10c8d72bd4aa9e0e654ad5b6d7d33fa4f7ab1
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-16 11:08:21 +0200

    profile fragment design draft

--- app/src/main/java/ntnu20/imt3673/group4/aves/FragmentProfile.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/FragmentProfile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class FragmentProfile : Fragment() {
@@ -24,6 +25,21 @@ class FragmentProfile : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val mA: MainActivity = activity as MainActivity
-        mA.fragName = "profile"
+        // TODO: mA.fragName = "profile" or similar. Wait until main activity is done.
+
+        /**
+         * Clickable text and buttons
+         */
+        btn_edit_profile.setOnClickListener{
+
+        }
+
+        txt_my_friends.setOnClickListener {
+
+        }
+
+        txt_my_observations.setOnClickListener {
+
+        }
     }
 }
