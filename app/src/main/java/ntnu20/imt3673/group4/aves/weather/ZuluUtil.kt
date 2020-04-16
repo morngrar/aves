1262cbcb3d9cabb092dfd4a4a2259e9174375380
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-16 16:17:51 +0200

    implemented getDifferenceBetween

--- app/src/main/java/ntnu20/imt3673/group4/aves/weather/ZuluUtil.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/weather/ZuluUtil.kt
@@ -4,18 +4,25 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.floor
+import kotlin.math.round
 
 object ZuluUtil {
 
     private val FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
+    private fun _getFormatter() : SimpleDateFormat {
+        val formatter = SimpleDateFormat(FORMAT_STRING)
+        formatter.timeZone = TimeZone.getTimeZone("UTC")
+        return formatter
+    }
+
     /**
      * Returns the custom zulu time string as used by the met.no API from given Date object
      */
     fun toZulu(date: Date) : String {
-        val formatter = SimpleDateFormat(FORMAT_STRING)
-        formatter.timeZone = TimeZone.getTimeZone("UTC")
-        return formatter.format(date)
+        return _getFormatter().format(date)
     }
 
     /**
@@ -23,15 +30,16 @@ object ZuluUtil {
      * the met.no API). Returns null if the string is invalid in some way.
      */
     fun toDate(zulu: String) : Date? {
-        val formatter = SimpleDateFormat(FORMAT_STRING)
-        formatter.timeZone = TimeZone.getTimeZone("UTC")
-        return formatter.parse(zulu)
+        return _getFormatter().parse(zulu)
     }
 
     /**
      * Returns the absolute difference between the zulu string and the date object in seconds
      */
     fun getDifferenceBetween(zulu: String, date: Date) : Long {
-        return 0
+        val zuluSeconds = _getFormatter().parse(zulu).time/1000
+        val dateSeconds = date.time/1000
+        val diff = round(zuluSeconds.toDouble() - dateSeconds.toDouble())
+        return abs(diff.toLong())
     }
 }
