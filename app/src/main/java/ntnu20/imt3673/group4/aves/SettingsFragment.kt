b9bea0f6a64ba3dd5f460c3bf5e6c387e7f84c38
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-17 16:38:25 +0200

    cleanup

--- app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
@@ -6,12 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
-import ntnu20.imt3673.group4.aves.databinding.FragmentSettingsBinding
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
-    private lateinit var views : FragmentSettingsBinding
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
