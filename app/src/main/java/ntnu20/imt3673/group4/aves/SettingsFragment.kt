74d33268d9118f75a003e0069dfacdf97593eb99
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-17 16:32:04 +0200

    Changed to non-deprecated settings screen + inflating works

--- app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
@@ -5,20 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import android.widget.Toast
+import androidx.preference.PreferenceFragmentCompat
 import ntnu20.imt3673.group4.aves.databinding.FragmentSettingsBinding
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
     private lateinit var views : FragmentSettingsBinding
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        views = FragmentSettingsBinding.inflate(inflater, container, false)
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
 }
