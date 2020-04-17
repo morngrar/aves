a2e2f2788174e3f4553209f28850e999a1985848
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-17 17:25:58 +0200

    xml parsing WORKS

--- app/src/main/java/ntnu20/imt3673/group4/aves/weather/WeatherUtil.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/weather/WeatherUtil.kt
@@ -1,12 +1,12 @@
 package ntnu20.imt3673.group4.aves.weather
 
-import android.util.Log
 import android.util.Xml
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserException
 import java.io.IOException
 import java.io.InputStream
 import java.util.*
+import kotlin.math.abs
 
 object WeatherUtil {
     // We don't use namespaces
@@ -14,9 +14,6 @@ object WeatherUtil {
 
     @Throws(XmlPullParserException::class, IOException::class)
     private fun parse(inputStream: InputStream): List<WeatherDataPoint> {
-        println("\n\n================================")
-        println("\nPARSE CALLED\n")
-        println("================================\n\n")
         inputStream.use { stream ->
             val parser: XmlPullParser = Xml.newPullParser()
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
@@ -37,8 +34,14 @@ object WeatherUtil {
             if (parser.eventType != XmlPullParser.START_TAG) {
                 continue
             }
-            // Starts by looking for the entry tag
+            // Starts by looking for the first time tag
             if (parser.name == "time") {
+                val from = ZuluUtil.toDate(readAttributeWithoutStepping(parser, "from"))
+                val to = ZuluUtil.toDate(readAttributeWithoutStepping(parser, "to"))
+                if (abs(from!!.time - to!!.time) == 21600000L) {
+                    skip(parser) // six hours span
+                    parser.nextTag()
+                }
                 points.add(readDataPoint(parser))
             } else {
                 skip(parser)
@@ -66,8 +69,6 @@ object WeatherUtil {
     @Throws(XmlPullParserException::class, IOException::class)
     private fun readDataPoint(parser: XmlPullParser): WeatherDataPoint {
         parser.require(XmlPullParser.START_TAG, ns, "time")
-        println("parser.name:")
-        println(parser.name)
 
         // define variables
         var altitude: String? = null             // meters above sea
@@ -90,27 +91,54 @@ object WeatherUtil {
         var precipitationMin: String? = null
 
         // Get time from time tag
-        var tag = parser.name
         val zuluTime = parser.getAttributeValue(null, "from")
 
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.eventType != XmlPullParser.START_TAG) {
                 continue
             }
-            println("parser.name:")
-            println(parser.name)
             when (parser.name) {
-                "location" -> altitude = null//readAltitude()
-                "temperature" -> temperature = readValue(parser, parser.name)
-                //"windDirection" -> windDirection = readName()
-                //"windSpeed" -> windSpeed = readMps()
-                //"windGust" -> windGust = readMps()
-                //"humidity" -> humidity = readValue()
+                "location" -> altitude = readAttributeWithoutStepping(parser, "altitude")
+                "temperature" -> temperature = readAttribute(parser, "value")
+                "windDirection" -> windDirection = readAttribute(parser, "name")
+                "windSpeed" -> windSpeed = readAttribute(parser, "mps")
+                "windGust" -> windGust =  readAttribute(parser, "mps")
+                "humidity" -> humidity = readAttribute(parser, "value")
+                "pressure" -> pressure = readAttribute(parser, "value")
+                "cloudiness" -> cloudiness = readAttribute(parser, "percent")
+                "fog" -> fog = readAttribute(parser, "percent")
+                "lowClouds" -> lowClouds = readAttribute(parser, "percent")
+                "mediumClouds" -> mediumClouds = readAttribute(parser, "percent")
+                "highClouds" -> highClouds = readAttribute(parser, "percent")
+                "dewpointTemperature" -> dewPoint = readAttribute(parser, "value")
                 else -> skip(parser)
             }
         }
 
-        val dataPoint = WeatherDataPoint(
+        // Getting precipitation
+        while (parser.name != "time") {
+            parser.nextTag()
+        }
+        parser.nextTag()
+        parser.require(XmlPullParser.START_TAG, ns, "time")
+        loop@ while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.eventType != XmlPullParser.START_TAG) {
+                continue
+            }
+            when (parser.name) {
+                "location" -> continue@loop
+                "precipitation" -> {
+                    precipitationValue = readAttributeWithoutStepping(parser, "value")
+                    precipitationMax = readAttributeWithoutStepping(parser, "maxvalue")
+                    precipitationMin = readAttributeWithoutStepping(parser, "minvalue")
+                    parser.next()
+                }
+                else -> skip(parser)
+            }
+        }
+
+
+        val point =  WeatherDataPoint(
             zuluTime,
             altitude,
             temperature,
@@ -129,29 +157,48 @@ object WeatherUtil {
             precipitationMax,
             precipitationMin
         )
-        println("TEMPERATURE:")
-        println(temperature)
-        return dataPoint
+        println(point.zuluTime)
+
+        parser.nextTag()
+        parser.require(XmlPullParser.END_TAG, ns, "time")
+
+        return point
     }
 
-    // returns the value of the 'value' attribute of a tag that has it
+    // Returns the value of the given attribute between location-tags
     @Throws(IOException::class, XmlPullParserException::class)
-    private fun readValue(parser: XmlPullParser, name: String): String {
-        parser.require(XmlPullParser.START_TAG, ns, name)
-        println("parser.name AFTER")
-        println(parser.name)
-        val value = parser.getAttributeValue(null, "value")
+    private fun readAttribute(parser: XmlPullParser, name: String): String {
+        parser.require(XmlPullParser.START_TAG, ns, parser.name)
+        val value = parser.getAttributeValue(null, name)
         parser.nextTag()
-        parser.require(XmlPullParser.END_TAG, ns, name)
+        parser.require(XmlPullParser.END_TAG, ns, parser.name)
         return value
     }
 
+    // Returns the value of the given attribute, without stepping the parser forward
+    @Throws(IOException::class, XmlPullParserException::class)
+    private fun readAttributeWithoutStepping(parser: XmlPullParser, name: String): String {
+        parser.require(XmlPullParser.START_TAG, ns, parser.name)
+        return parser.getAttributeValue(null, name)
+    }
+
+
     fun getNearestDataPoint(date: Date, xmlData: String): WeatherDataPoint? {
-
         val input = xmlData.byteInputStream()
-
         val pointList = parse(input)
 
-        return pointList[0]
+        var pointData: WeatherDataPoint? = null
+        for (point in pointList) {
+            //println(point)
+            val pointDate = ZuluUtil.toDate(point.zuluTime)
+            if (pointDate != null) {
+                if (pointDate.after(date)) {
+                    pointData = point
+                    break
+                }
+            }
+        }
+
+        return pointData
     }
 }
