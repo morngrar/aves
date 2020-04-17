4520e2038323f22d9483936342200d5f04aa1ea8
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-17 18:04:23 +0200

    cleanup

--- app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
@@ -1,15 +1,9 @@
 package ntnu20.imt3673.group4.aves
 
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
-
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
