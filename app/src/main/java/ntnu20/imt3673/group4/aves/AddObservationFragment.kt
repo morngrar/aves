acfa34dd13f2312c0661d507f75f02c6aaf9dd2e
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-18 17:21:48 +0200

    Added basic layout for adding observations

---
+++ app/src/main/java/ntnu20/imt3673/group4/aves/AddObservationFragment.kt
@@ -0,0 +1,32 @@
+package ntnu20.imt3673.group4.aves
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AddObservationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddObservationFragment()
+    }
+
+    private lateinit var viewModel: AddObservationViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.add_observation_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AddObservationViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ntnu20/imt3673/group4/aves/AddObservationViewModel.kt
@@ -0,0 +1,7 @@
+package ntnu20.imt3673.group4.aves
+
+import androidx.lifecycle.ViewModel
+
+class AddObservationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
