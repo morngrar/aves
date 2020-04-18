446204ac93fcfa6f17f62e1d819561c4a11296bb
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-18 13:40:43 +0200

    light sensor works in MainActivity

--- app/src/main/java/ntnu20/imt3673/group4/aves/MainActivity.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/MainActivity.kt
@@ -1,5 +1,10 @@
 package ntnu20.imt3673.group4.aves
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,13 +16,18 @@ import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI
 import androidx.preference.PreferenceManager
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.fragment_activity_main.*
 
 
 /**
  * This is an example of how the location utility is to be used. It is important that this is
  * used in a short-lived activity for registering gps data, or it will affect battery life.
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var light: Sensor? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +50,10 @@ class MainActivity : AppCompatActivity() {
         // val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         // var useLocation = sharedPreferences.getBoolean("pref_location", false)
 
+        /* Light sensor */
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
     }
     /** Sets up the navigation menu to their respective dests*/
     private fun setUpNavigationMenu(navController: NavController) {
@@ -56,5 +70,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Not implemented. Not Needed. 
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            txt_hello.text = ("Lux: " + event.values[0])
+        }
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
 }
 
