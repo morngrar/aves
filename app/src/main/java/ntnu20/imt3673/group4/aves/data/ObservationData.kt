8509b9901c6068dd471d86db1e4981fe2859627d
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-18 15:57:14 +0200

    Added database skeleton

---
+++ app/src/main/java/ntnu20/imt3673/group4/aves/data/ObservationData.kt
@@ -0,0 +1,19 @@
+package ntnu20.imt3673.group4.aves.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "observations")
+data class ObservationData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long,
+    val birdName: String,
+    val imagePath: String,
+    val time: Long,
+    val latitude: Double,
+    val longitude: Double,
+    val rain: String,
+    val windSpeed: String,
+    val cloudiness: String,
+    val pressure: String   // probably better to use the onboard sensor if available
+)
