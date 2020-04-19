927062a2e5f21a6095ff9c33b0e6fb3865e9400c
Svein-Kåre Bjørnsen <sveinkab@stud.ntnu.no>
2020-04-19 18:20:53 +0200

    new cards for recyclerview

--- app/src/main/java/ntnu20/imt3673/group4/aves/MainActivityFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/MainActivityFragment.kt
@@ -1,49 +1,30 @@
 package ntnu20.imt3673.group4.aves
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.fragment_activity_main.*
-import ntnu20.imt3673.group4.aves.adapter.ObservationAdapter
-import ntnu20.imt3673.group4.aves.dataSource.dummyData
+import ntnu20.imt3673.group4.aves.adapters.ObservationAdapter
 import ntnu20.imt3673.group4.aves.databinding.FragmentActivityMainBinding
 
 
 class MainActivityFragment : Fragment() {
 
-    private lateinit var views : FragmentActivityMainBinding
-    private lateinit var observationAdapter: ObservationAdapter
+    private lateinit var binding : FragmentActivityMainBinding
+
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        views = FragmentActivityMainBinding.inflate(inflater, container, false)
-        return views.root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_activity_main, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        /* Initiates the recycler view*/
-        initRecyclerView()
-        addDataSet()
-    }
-
-    private fun addDataSet() {
-        val data = dummyData.createDataSet()
-        observationAdapter.submitList(data)
-    }
-
-    /**
-     * Function initiates the recycler view and applies the adapter
-     */
-    private fun initRecyclerView() {
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            observationAdapter = ObservationAdapter()
-            adapter = observationAdapter
+        binding.recyclerView.apply {
+            adapter = ObservationAdapter()
+            layoutManager = LinearLayoutManager(context)
         }
     }
 }
