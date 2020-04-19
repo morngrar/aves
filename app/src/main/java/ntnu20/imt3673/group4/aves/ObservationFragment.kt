6c524b3462fbc818efd26d701e5eacca788b3384
charleyv <charleyv@stud.ntnu.no>
2020-04-19 12:52:37 +0200

    Functioning add to internal gallery

--- app/src/main/java/ntnu20/imt3673/group4/aves/ObservationFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/ObservationFragment.kt
@@ -1,24 +1,29 @@
 package ntnu20.imt3673.group4.aves
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_observation.*
 import ntnu20.imt3673.group4.aves.databinding.FragmentObservationBinding
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -51,18 +56,14 @@ class ObservationFragment : Fragment() {
         //Button to save image captured
         btn_share.setOnClickListener {
             if(image_view.drawable != null) {
-
-                // Save the image to gallery and get saved image
-
-
+                // Save the image to gallery
+                galleryAddPic()
             } else {
                 Toast.makeText(requireContext(), "Take a picture ", Toast.LENGTH_SHORT).show()
             }
         }
 
     }
-
-
 
     /**
      * Function checks if the user has allowed access to camera and storage
@@ -82,7 +83,6 @@ class ObservationFragment : Fragment() {
             requestPermissions(permission, requestCode)
         } else {
             // If the permission was already granted, capture the image
-//            captureImage()
             dispatchTakePictureIntent()
         }
     }
@@ -102,7 +102,7 @@ class ObservationFragment : Fragment() {
                 photoFile?.also {
                     val photoURI: Uri = FileProvider.getUriForFile(
                         requireContext(),
-                        "com.example.android.fileprovider",
+                        "ntnu20.imt3673.group4.aves.provider",
                         it
                     )
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
@@ -112,6 +112,7 @@ class ObservationFragment : Fragment() {
         }
     }
 
+    @SuppressLint("SimpleDateFormat")
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
@@ -133,105 +134,19 @@ class ObservationFragment : Fragment() {
         }
     }
 
-//    private fun galleryAddPic() {
-//        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
-//            val f = File(currentPhotoPath)
-//            mediaScanIntent.data = Uri.fromFile(f)
-//            sendBroadcast(mediaScanIntent)
-//        }
-//    }
-/**
-    private fun takePicture() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        if(intent.resolveActivity(requireActivity().packageManager) != null) {
-            var photoFile: File? = null
-
-            try {
-                photoFile = createImageFile()
-            } catch (e: IOException) {}
-            if(photoFile != null) {
-                val photoUri = FileProvider.getUriForFile(
-                    requireContext(),
-                    "com.example.android.fileprovider",
-                    photoFile
-                )
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-                startActivityForResult(intent, requestCode)
-            }
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_VIEW).also { mediaScanIntent ->
+            val ctx = requireContext()
+            mediaScanIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = FileProvider.getUriForFile(ctx, ctx.applicationContext.packageName + ".provider", f)
+            requireContext().sendBroadcast(mediaScanIntent)
+            Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
         }
     }
-
-    private fun createImageFile() : File? {
-        val fileName = "MyPicture"
-        val storageDir = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            fileName,
-            ".jpg",
-            storageDir
-        )
-        photoPath = image.absolutePath
-        return image
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == requestCode && resultCode == Activity.RESULT_OK) {
-
-            image_view.setImageURI(Uri.parse(photoPath))
-        }
-    }
-    **/
-//
-//
-//    private fun captureImage() {
-//        // Camera intent
-//        val captureImageIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        startActivityForResult(captureImageIntent, requestCode)
-//    }
-//
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//
-//        //When User Presses Allow Or Deny
-//        when (requestCode) {
-//            this.requestCode -> {
-//                // If user grants access
-//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-//                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
-//                ) {     // Take picture
-//                    captureImage()
-//                } else {
-//                    Toast.makeText(requireContext(), "Permission denied, check application permission settings", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//            else -> {
-//                Toast.makeText(requireContext(), "Unrecognizable request code", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
-//
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        when (requestCode) {
-//            this.requestCode -> {
-//                if (resultCode == Activity.RESULT_OK && data != null) {
-////                    image_view.setImageBitmap(data.extras?.get("data") as Bitmap)
-//                    val imageBitmap = data.extras.get("data") as Bitmap
-//                    image_view.setImageBitmap(imageBitmap)
-//                }
-//            }
-//            else -> {
-//                Toast.makeText(requireContext(), "Unrecognizable request code", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//        }
-//    }
 }
+
+
 
 
 
