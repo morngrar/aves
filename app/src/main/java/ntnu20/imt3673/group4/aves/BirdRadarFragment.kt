10698d3d3b3433d982e13b41d82ced702cfb4ab4
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-30 14:50:44 +0200

    working on adding markers to map

--- app/src/main/java/ntnu20/imt3673/group4/aves/BirdRadarFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/BirdRadarFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 
 class BirdRadarFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
+
+
     }
 
     override fun onCreateView(
@@ -22,5 +29,18 @@ class BirdRadarFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bird_radar, container, false)
+    }
+
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+            )
+        }
     }
 }
