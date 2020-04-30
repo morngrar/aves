90ed06b3e1e787f0fc090618ac22700c2fdf2e6f
Ruslebiff <joakivb@stud.ntnu.no>
2020-04-30 13:52:59 +0200

    removed 'use location' preference from settings

--- app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/SettingsFragment.kt
@@ -12,21 +12,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
-        /**
-         * Location:
-         * Change description of summary for the location preference
-         */
-        val locationPreference: SwitchPreferenceCompat? = findPreference("pref_location")
-        locationPreference?.summaryProvider =
-            Preference.SummaryProvider<SwitchPreferenceCompat> { preference ->
-                if (preference.isChecked) {
-                    "Location is enabled"
-                } else {
-                    "Location is disabled"
-                }
-
-            }
-
         /** Button to apply changes in the settings */
         val restartButton: Preference? = findPreference("btn_restart_app")
         restartButton!!.onPreferenceClickListener =
