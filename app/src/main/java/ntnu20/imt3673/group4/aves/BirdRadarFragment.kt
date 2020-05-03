dfc809860eabb533e3a0fb762ad429af0ee3df37
Ruslebiff <joakivb@stud.ntnu.no>
2020-05-03 12:03:30 +0200

    cleanup, removed unused files

--- app/src/main/java/ntnu20/imt3673/group4/aves/BirdRadarFragment.kt
+++ app/src/main/java/ntnu20/imt3673/group4/aves/BirdRadarFragment.kt
@@ -7,16 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
-import ntnu20.imt3673.group4.aves.data.ObservationData
 import ntnu20.imt3673.group4.aves.data.toPlace
 import ntnu20.imt3673.group4.aves.place.Place
-import ntnu20.imt3673.group4.aves.place.PlacesReader
 import ntnu20.imt3673.group4.aves.viewmodels.FirestoreViewModel
 
 
@@ -25,24 +22,10 @@ class BirdRadarFragment : Fragment() {
         Application()
     ).create(FirestoreViewModel::class.java)
 
-    val allObservations = firestoreViewModel.getSavedObservations()
-
-    private lateinit var mMap: GoogleMap
-//    private val places: List<Place> by lazy {
-//        PlacesReader(this.requireContext()).read()
-//    }
+    private val allObservations = firestoreViewModel.getSavedObservations()
 
     private lateinit var places: MutableList<Place>
 
-    private val observationDatas: MutableLiveData<ObservationData> by lazy {
-        MutableLiveData<ObservationData>()
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,28 +38,15 @@ class BirdRadarFragment : Fragment() {
         fragmentTransaction.add(R.id.map_fragment, mapFragment)
         fragmentTransaction.commit();
 
-        val observationDataObserver = Observer<ObservationData> {
-            Log.d("AEAEobs: ", it.toString())
-            it.toPlace()
-        }
-
         places = mutableListOf<Place>()
-        
+
         allObservations.observe(viewLifecycleOwner, Observer { it ->
             it.forEach {
-                Log.d("AEAEffff ", it.toPlace().toString())
-
                 places.add(it.toPlace())
-                Log.d("AEAEPlaces: ", places.toString())
             }
         })
 
-
         mapFragment.getMapAsync { googleMap -> addMarkers(googleMap) }
-
-
-
-
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bird_radar, container, false)
@@ -91,7 +61,7 @@ class BirdRadarFragment : Fragment() {
         Log.d("addMarkers ", googleMap.toString())
         places.forEach { place ->
             Log.d("Added new place: ", place.toString())
-            val marker = googleMap.addMarker(
+            googleMap.addMarker(
                 MarkerOptions()
                     .title(place.name)
                     .position(place.latLng)
